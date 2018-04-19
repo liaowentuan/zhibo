084ba71755fef9b9b20b177b5675506c02cd5a24
liaowentuan <996002654@qq.com>
2018-04-19 15:27:29 +0800

    node版本

---
+++ node-server/server.js
@@ -0,0 +1,46 @@
+Stream = require('node-rtsp-stream');
+// stream0 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9990
+// });
+// stream1 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9991
+// });
+// stream2 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9992
+// });
+// stream3 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9993
+// });
+// stream4 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9994
+// });
+// stream5 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.12:554/h264/ch1/main/av_stream',
+//     wsPort: 9995
+// });
+// stream6 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9996
+// });
+// stream7 = new Stream({
+//     name: 'name',
+//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+//     wsPort: 9997
+// });
+stream8 = new Stream({
+    name: 'name',
+    streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
+    wsPort: 9999
+});
