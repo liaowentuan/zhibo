4f4da8ea77237850cb90f9ab422eac7fe1cf3892
liaowentuan <996002654@qq.com>
2018-04-20 16:55:38 +0800

    new

--- node-server/server.js
+++ node-server/server.js
@@ -1,46 +1,31 @@
-Stream = require('node-rtsp-stream');
-// stream0 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9990
-// });
-// stream1 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9991
-// });
-// stream2 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9992
-// });
-// stream3 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9993
-// });
-// stream4 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9994
-// });
-// stream5 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.12:554/h264/ch1/main/av_stream',
-//     wsPort: 9995
-// });
-// stream6 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9996
-// });
-// stream7 = new Stream({
-//     name: 'name',
-//     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
-//     wsPort: 9997
-// });
+// http://127.0.0.1:61234/server?rtsp=rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream&port=9999
+
+/*
+
 stream8 = new Stream({
     name: 'name',
     streamUrl: 'rtsp://admin:lst12345678@10.10.88.11:554/h264/ch1/main/av_stream',
     wsPort: 9999
-});
+});*/
+const Stream = require('node-rtsp-stream');
+const http = require('http');
+const url  = require('url');
+
+http.createServer((req,res)=>{
+	// res.writeHead(200, { 'Content-Type': 'text-plain' });
+    query = url.parse(req.url,true).query
+    let rtsp = query.rtsp
+    let port = query.port
+    console.log(rtsp,port)
+    if(rtsp){
+    	stream = new Stream({
+		    name: 'name',
+		    streamUrl: rtsp,
+		    wsPort: port
+		});
+    }
+    //利用url模块去解析客户端发送过来的URL
+    res.end('hello world\n');
+    
+}).listen(61234)
+
