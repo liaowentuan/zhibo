3f2d5ed1364f55d782faaf8fa9be4c68cc43a896
liaowentuan <996002654@qq.com>
2018-04-24 12:41:35 +0800

    node_module 从仓库拷贝

--- node-server/server.js
+++ node-server/server.js
@@ -12,11 +12,33 @@ const http = require('http');
 const url  = require('url');
 
 http.createServer((req,res)=>{
+	let rtsp
+	let port
+	let close
 	// res.writeHead(200, { 'Content-Type': 'text-plain' });
     query = url.parse(req.url,true).query
-    let rtsp = query.rtsp
-    let port = query.port
-    console.log(rtsp,port)
+    if(query.rtsp){
+    	rtsp = query.rtsp
+    }
+    if(query.port){
+    	port = query.port
+    }
+    if(query.close){
+    	close = query.close
+    	rtsp = undefined
+    	port = undefined
+    	try {
+   			stream = new Stream({
+			    name: 'name',
+			    streamUrl: rtsp,
+			    wsPort: port
+			});
+		}
+		catch(err){
+			stream.mpeg1Muxer.stream.kill();
+		}
+    }
+
     if(rtsp){
     	stream = new Stream({
 		    name: 'name',
